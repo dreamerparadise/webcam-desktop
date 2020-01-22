7233f51d7543364b47fee453940df090d57edb3b
tutoraddicts <rohitdutta907@gmail.com>
2020-01-23 01:37:53 +0530

    updated better explaination explaination of codes

--- main.py
+++ main.py
@@ -15,25 +15,33 @@ class MyGrid(GridLayout):
 		super(MyGrid, self).__init__(**kwargs)
 		#set colom of main gridlayout as 1
 		self.cols = 1
+
 		#Creating a new grid layout name as Information
 		self.Information = GridLayout()
+
 		#set the coloms of Informtaion to 1
 		self.Information.cols = 1
 		information = "First You Need To Download IPWebcam From Playstore."
 		self.Information.add_widget(Label(text = information))
+
 		#Add the Information Layout into the main Layout
 		self.add_widget(self.Information)
+
 		#Againg creat a new Layout name as getData
 		self.getData = GridLayout()
+
 		#set cols number of getData
 		self.getData.cols = 2
 		self.getData.add_widget(Label(text= "Enter Camera Ip"))
+
 		#Addind Text Panel to have the text
 		self.url = TextInput(multiline = False)
 		self.getData.add_widget(self.url)
 		self.add_widget(self.getData)
+
 		#Adding Buttton TO get The Ip of camera
 		self.submit = Button(text="submit")
+
 		#Adding Option pressed so if the button pressed all the things happends what i want
 		self.submit.bind(on_press=self.pressed)
 		self.add_widget(self.submit)
@@ -41,6 +49,7 @@ class MyGrid(GridLayout):
 
 	def pressed(self, instance):
 		print("Pressed")
+		
 		#Started FaceDetection Work
 		#Prepear the Url To work Perfectly
 		if self.url.text:
@@ -50,14 +59,29 @@ class MyGrid(GridLayout):
 			while True:
 				ret, frame = video_capture.read()
 				frame = cv2.resize(frame, None, fx=0.2, fy=0.2)
+
+				#Converting to RGB color scheme because cv2 uses BGR color scheme
 				rgb_frame = frame[:, :, ::-1]
+
+				#face_location is an array where we are collection face location axis in every frame.
 				face_locations = face_recognition.face_locations(rgb_frame)
 
+				#in this for loop we are using four axis data we collect in face_location array
+				#loop will continue untill the data face array data finishes
+				#the array data is = the total no of frames we collected.
 				for top, right, bottom, left in face_locations:
+					#cv2.rectangle is a funtion to draw rectangle in a perticular axis of a picture
+					#basically we are just drawing rectangle with the color (0,0,255) in every frame
 					cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 2)
+				
+				#cv2.show is a cv2 function the show the frames one by one
 				cv2.imshow('Video', frame)
+
+				#waiting for the q to be press then we will exit.
 				if cv2.waitKey(1) & 0xFF == ord('q'):
 					break
+			
+			#video_capture.release() --> is to release the variable which is capturing the the data from webcam
 			video_capture.release()
 			cv2.destroyAllWindows()
 		else:
@@ -67,6 +91,8 @@ class MyGrid(GridLayout):
 			self.submit.bind(on_press=self.pressed)
 
 class MyApp(App):
+	
+	#build is a pre defines methon
 	def build(self):
 		return MyGrid()
 
