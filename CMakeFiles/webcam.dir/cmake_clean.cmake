cb0a709e2c020688c9190d058cc09399ea56b761
tutoraddicts <rohitdutta907@gmail.com>
2020-10-27 01:29:50 +0530

    first Commit for C++

---
+++ CMakeFiles/webcam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webcam.dir/src/webcam.cpp.o"
+  "webcam"
+  "webcam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webcam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
